a97a399459446f9f4cbb7f4da30867e8f7b7dc84
Jesse McKay and Margie Nevarez <jesse@>
2016-10-25 14:33:46 -0700

    separated business and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,50 @@
 
+var finishedSentence = function(sentence) {
+  var letters = sentence.split("");
+  var newSentence = [];
+
+  for (var index = 0; index < letters.length; index += 1) {
+    //var newSentence = sentence.replace(vowels[index], "-");
+    if (letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" || letters[index] === "u") {
+      newSentence.push("-");
+    } else {
+      newSentence.push(letters[index]);
+    }
+  }
+  var output = newSentence.join("");
+  return output;
+}
+
 $(document).ready(function(){
   $("form#puzzle").submit(function(event){
     event.preventDefault();
-    var vowels = ["a", "e", "i", "o", "u"];
     var sentence = $("#sentence").val();
-    var letters = sentence.split("");
-    var newSentence = [];
-
-    for (var index = 0; index < letters.length; index += 1) {
-      //var newSentence = sentence.replace(vowels[index], "-");
-      if (letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" || letters[index] === "u") {
-        newSentence.push("-");
-      } else {
-        newSentence.push(letters[index]);
-      }
-    }
-    var lettersJoined = newSentence.join("");
-    // alert(lettersJoined);
-
+    var lettersJoined = finishedSentence(sentence);
     $("p").text(lettersJoined);
-
   });
 });
+
+
+// $(document).ready(function(){
+//   $("form#puzzle").submit(function(event){
+//     event.preventDefault();
+//     // var vowels = ["a", "e", "i", "o", "u"];
+//     var sentence = $("#sentence").val();
+//     var letters = sentence.split("");
+//     var newSentence = [];
+//
+//     for (var index = 0; index < letters.length; index += 1) {
+//       //var newSentence = sentence.replace(vowels[index], "-");
+//       if (letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" || letters[index] === "u") {
+//         newSentence.push("-");
+//       } else {
+//         newSentence.push(letters[index]);
+//       }
+//     }
+//     var lettersJoined = newSentence.join("");
+//     // alert(lettersJoined);
+//
+//     $("p").text(lettersJoined);
+//
+//   });
+// });
