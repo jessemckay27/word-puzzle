c4546947140a3d06921dfcaddc721da09ad94cb4
Jesse McKay and Margie Nevarez <margie@>
2016-10-25 13:35:07 -0700

    working, not separated business and front end

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+  $("form#puzzle").submit(function(event){
+    event.preventDefault();
+    var vowels = ["a", "e", "i", "o", "u"];
+    var sentence = $("#sentence").val();
+    var letters = sentence.split("");
+    var newSentence = [];
+
+    for (var index = 0; index < letters.length; index += 1) {
+      //var newSentence = sentence.replace(vowels[index], "-");
+      if (letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" || letters[index] === "u") {
+        newSentence.push("-");
+      } else {
+        newSentence.push(letters[index]);
+      }
+    }
+    var lettersJoined = newSentence.join("");
+    alert(lettersJoined);
+
+  });
+});
